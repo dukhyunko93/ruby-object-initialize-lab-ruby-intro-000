072bdc1bd9ef5c8d49e6c3144aaa9c02a804c1bd
Duke Ko <dukhyunko93@gmail.com>
2020-03-24 23:56:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 class Dog
 
+<<<<<<< HEAD
   def initialize(name, breed="Mutt")
     @name = name
     @breed = breed
@@ -7,18 +8,35 @@ class Dog
 
   def name
     @name
+=======
+  def initialize(name, breed)
+    @name = name
+    @breed = Mutt
+>>>>>>> ceabc46ef2d3d74254ba279c878d9d9b5bd5ef4d
   end
 
   def name=(name)
     @name = name
   end
 
+<<<<<<< HEAD
   def breed
     @breed
+=======
+  def name
+    @name
+>>>>>>> ceabc46ef2d3d74254ba279c878d9d9b5bd5ef4d
   end
 
   def breed=(breed)
     @breed = Mutt
   end
 
+<<<<<<< HEAD
+=======
+  def breed
+    @breed
+  end
+
+>>>>>>> ceabc46ef2d3d74254ba279c878d9d9b5bd5ef4d
 end
--- lib/person.rb
+++ lib/person.rb
@@ -4,12 +4,22 @@ class Person
     @name = name
   end
 
+<<<<<<< HEAD
   def name
     @name
   end
 
+=======
+>>>>>>> ceabc46ef2d3d74254ba279c878d9d9b5bd5ef4d
   def name=(name)
     @name = name
   end
 
+<<<<<<< HEAD
+=======
+  def name
+    @name
+  end
+
+>>>>>>> ceabc46ef2d3d74254ba279c878d9d9b5bd5ef4d
 end
